23d86af2a1e82881b972236c8e7b9a5a40d3cc1b
Maarten Goudsmit <maartengoudsmit@gmail.com>
2026-02-22 17:39:35 +0100

    Update permalinks

--- content/notes/notes.11tydata.js
+++ content/notes/notes.11tydata.js
@@ -1,6 +1,7 @@
 export default {
   tags: "note",
   layout: "noteDetail.liquid",
+  permalink: "notes/{{page.fileSlug}}/",
   eleventyComputed: {
     note: (data) => data.collections.note?.find((n) => n.url === data.page.url),
   },
--- content/reviews/reviews.11tydata.js
+++ content/reviews/reviews.11tydata.js
@@ -1,3 +1,5 @@
 export default {
   tags: "review",
+  layout: "reviewDetail.liquid",
+  permalink: "reviews/",
 };
