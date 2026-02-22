f1635037e85d49c3aefd209cfc23b36bbdc65360
Maarten Goudsmit <maartengoudsmit@gmail.com>
2026-02-22 21:26:19 +0100

    Create tag collection pages

---
+++ tag.11tydata.js
@@ -0,0 +1,19 @@
+export default {
+  pagination: {
+    data: "collections.all",
+    size: 1,
+    alias: "tag",
+    before: function (data) {
+      const excluded = ["note", "review"];
+      const tags = new Set();
+      data.forEach((item) => {
+        (item.data.tags || []).forEach((tag) => {
+          if (!excluded.includes(tag)) tags.add(tag);
+        });
+      });
+      return [...tags];
+    },
+    addAllPagesToCollection: false,
+  },
+  permalink: "/tag/{{ tag | slugify }}/",
+};
