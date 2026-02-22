083ce509a8eb5de6ba733019a4d6bdc6fa1aa146
Maarten Goudsmit <maartengoudsmit@gmail.com>
2026-02-22 15:15:17 +0100

    Add 11ty image plugin

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,7 +1,10 @@
+import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
+
 export default function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./styles/");
   eleventyConfig.addWatchTarget("./styles/");
   eleventyConfig.addPassthroughCopy("./scripts/");
   eleventyConfig.addWatchTarget("./scripts/");
   eleventyConfig.addPassthroughCopy("./admin/");
+  eleventyConfig.addPlugin(eleventyImageTransformPlugin);
 }
