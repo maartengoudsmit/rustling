5266463b579164fc97247ff0bbcb24ae4b883998
Maarten Goudsmit <maartengoudsmit@gmail.com>
2026-02-23 09:45:16 +0100

    Add relative date filter + implement in note and review template

--- eleventy.config.js
+++ eleventy.config.js
@@ -11,4 +11,29 @@ export default function (eleventyConfig) {
     const hostname = new URL(url).hostname.replace("www.", "");
     return `<a href="${url}">${hostname}</a>`;
   });
+
+  eleventyConfig.addFilter("relative_date", (input) => {
+    const date = new Date(input);
+    const pastDate = new Date(date);
+    const difference = pastDate - Date.now();
+    const rel = new Intl.RelativeTimeFormat("en", {
+      style: "long",
+      numeric: "auto",
+    });
+    const relString = (formatter, value, unit) =>
+      `${formatter.format(Math.round(value), unit)}`;
+
+    const seconds = difference / 1000;
+    const minutes = seconds / 60;
+    const hours = minutes / 60;
+    const days = hours / 24;
+    const weeks = days / 7;
+    const years = days / 365;
+
+    if (years < -1) return relString(rel, years, "year");
+    else if (weeks < -1) return relString(rel, weeks, "week");
+    else if (days < -1) return relString(rel, days, "day");
+    else if (hours < -1) return relString(rel, hours, "hour");
+    else return relString(rel, minutes, "minute");
+  });
 }
