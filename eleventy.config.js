2c49a5be4c79c18443606fb45d656c37c8272b4f
Maarten Goudsmit <maartengoudsmit@gmail.com>
2026-02-24 22:48:05 +0100

    Add comments for better documentation

--- eleventy.config.js
+++ eleventy.config.js
@@ -12,12 +12,15 @@ export default function (eleventyConfig) {
   eleventyConfig.addPlugin(eleventyImageTransformPlugin, {
     formats: ["avif", "jpeg"],
   });
+
+  // Turns `https://www.imdb.com/title/tt12345` into `imdb.com`
   eleventyConfig.addShortcode("hostname", function (url) {
     const hostname = new URL(url).hostname.replace("www.", "");
     return `<a href="${url}">${hostname}</a>`;
   });
 
   eleventyConfig.addFilter("relative_date", (input) => {
+    // Embed date into span element for easy retrieval at run time
     return `<span class="relative-date" data-date="${input}">${relativeDate(
       input,
     )}</span>`;
