481d7533e13addeadd31134dfe457c950cf01b73
Maarten Goudsmit <maartengoudsmit@gmail.com>
2026-02-25 14:28:29 +0100

    Move draft exclusion logic to preprocessor

--- eleventy.config.js
+++ eleventy.config.js
@@ -3,15 +3,10 @@ import syntaxHighlight from "@11ty/eleventy-plugin-syntaxhighlight";
 import { relativeDate } from "./scripts/relativeDate.js";
 
 export default function (eleventyConfig) {
-  eleventyConfig.addGlobalData("eleventyComputed", {
-    permalink(data) {
-      if (data.draft && data.eleventy.env.runMode === "build") return false;
-      return data.permalink;
-    },
-    eleventyExcludeFromCollections(data) {
-      if (data.draft && data.eleventy.env.runMode === "build") return true;
-      return data.eleventyExcludeFromCollections;
-    },
+  eleventyConfig.addPreprocessor("drafts", "*", (data, content) => {
+    if (data.draft && process.env.ELEVENTY_RUN_MODE === "build") {
+      return false;
+    }
   });
 
   eleventyConfig.addPassthroughCopy("./styles/");
