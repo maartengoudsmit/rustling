04595d445e523b38d954d7fc9da06fb2d60b890e
Maarten Goudsmit <maartengoudsmit@gmail.com>
2026-02-26 14:47:06 +0100

    Revert to default template rendering

--- content/notes/notes.11tydata.js
+++ content/notes/notes.11tydata.js
@@ -3,7 +3,7 @@ export default {
   dir: "note", // Used for permalinks
   layout: "noteDetail.liquid",
   // Prevent template engine from interpreting HTML and Javascript in Markdown files
-  templateEngineOverride: "md",
+  // templateEngineOverride: "md",
   eleventyComputed: {
     // Ensures the template has a uniform object, even outside a for loop
     note: (data) => data.collections.note?.find((n) => n.url === data.page.url),
